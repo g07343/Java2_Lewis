c03e756b6c825e11a38c57443ddd5e652d556a80
Matthew Lewis <Loneeagle14@aol.com>
2014-06-04 00:36:10 -0500

    Service implemented
    Got service up and running.  Currently only receives and passes back
    the URL for our API (as a test to make sure it works), so next we need
    to have it actually pull down data with the url.  Also, redid the
    earlier handler to utilize an inner class to avoid memory leaks as per
    the project rubric requirements.

--- Week 1/SportsCaster/src/com/matthewlewis/sportscaster/APIService.java
+++ Week 1/SportsCaster/src/com/matthewlewis/sportscaster/APIService.java
@@ -13,11 +13,20 @@
 
 package com.matthewlewis.sportscaster;
 
+import android.app.Activity;
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
 
 public class APIService extends IntentService{
 
+	public static final String MESSENGER_KEY = "messenger";
+	public static final String API_KEY = "api";
+	
 	public APIService() {
 		super("APIService");
 		// TODO Auto-generated constructor stub
@@ -26,7 +35,22 @@ public class APIService extends IntentService{
 	@Override
 	protected void onHandleIntent(Intent intent) {
 		// TODO Auto-generated method stub
+		Log.i("ON_HANDLE_INTENT","service has started..");
 		
+		Bundle extras = intent.getExtras();
+		Messenger messenger = (Messenger) extras.get(MESSENGER_KEY);
+		String url = extras.getString(API_KEY);
+		
+		Message msg = Message.obtain();
+		msg.arg1 = Activity.RESULT_OK;
+		msg.obj = "Url sent was:  " + url;
+		try {
+			messenger.send(msg);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Log.e("ON_HANDLE_INTENT", "error sending message");
+		}
 	}
 
 }
--- Week 1/SportsCaster/src/com/matthewlewis/sportscaster/MainActivity.java
+++ Week 1/SportsCaster/src/com/matthewlewis/sportscaster/MainActivity.java
@@ -12,74 +12,95 @@
  */
 package com.matthewlewis.sportscaster;
 
+import java.lang.ref.WeakReference;
+
 import android.app.Activity;
-import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.matthewlewis.sportscaster.NetworkManager;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        NetworkManager manager = new NetworkManager();
-        Boolean connected = manager.connectionStatus(this);
-        TextView warningField = (TextView) findViewById(R.id.internet_warning);
-        if (connected)
-        {
-        	Log.i("CONNECTION_CHECK", "Good connection");
-        	warningField.setText("Internet Connected!");
-        } else {
-        	Log.i("CONNECTION_CHECK", "No connection");
-        	warningField.setText("No internet!");
-        }
-    }
+	private static String apiURL = "http://api.espn.com/v1/now";
+	private static TextView statusField;
+	private static Context context;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		context = this;
+		statusField  = (TextView) findViewById(R.id.internet_warning);
+		NetworkManager manager = new NetworkManager();
+		Boolean connected = manager.connectionStatus(this);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		if (connected) {
+			Log.i("CONNECTION_CHECK", "Good connection");
+			statusField.setText("Internet Connected!");
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+			final ApiHandler apiHandler = new ApiHandler(this);
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+			Messenger apiMessenger = new Messenger(apiHandler);
+			Intent startApiService = new Intent(context, APIService.class);
+			startApiService.putExtra(APIService.MESSENGER_KEY, apiMessenger);
+			startApiService.putExtra(APIService.API_KEY, apiURL);
+			startService(startApiService);
+			statusField.setText("Getting data...");
 
-        public PlaceholderFragment() {
-        }
+		} else {
+			Log.i("CONNECTION_CHECK", "No connection");
+			statusField.setText("No internet!");
+		}
+	}
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private static class ApiHandler extends Handler {
+		private final WeakReference<MainActivity> mActivity;
+
+		public ApiHandler(MainActivity activity) {
+			mActivity = new WeakReference<MainActivity>(activity);
+		}
+
+		@Override
+		public void handleMessage(Message msg) {
+			MainActivity activity = mActivity.get();
+			if (activity != null) {
+				String response;
+				if (msg.arg1 == RESULT_OK && msg.obj != null)
+				{
+					response = (String) msg.obj;
+					Log.i("HANDLE_MESSAGE", response);
+				}
+			}
+		}
+	}
 
 }
